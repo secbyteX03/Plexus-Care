3f0c45e3f80590f3c098c0e86730624ffd4a407d
secbyteX03 <secbyteX03@users.noreply.github.com>
2025-09-03 20:28:06 +0300

    chore: remove js/config.js from version control and update .gitignore

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Configuration for Supabase
-const SUPABASE_URL = 'https://vkfntjnhanpaiuhpkroh.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZm50am5oYW5wYWl1aHBrcm9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NjY0NzMsImV4cCI6MjA3MjE0MjQ3M30.aGURsdU6nVv6XDUioeGOvVnApBnNcMTiORrkl1N5KlI';
-
-export { SUPABASE_URL, SUPABASE_ANON_KEY };
